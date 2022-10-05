13782997b79ec56d3adccb6f0a775c55983dd5f2
CSI20-JCBC1 <csi20-jbadrbo@colegioaltair.net>
2022-10-05 12:26:21 +0200

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Conexiones/ConexionPostgreSQL.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models.Conexiones
+{
+    public class ConexionPostgreSQL
+    {
+
+
+
+    }
+}
---
+++ WebApplication1/Models/DTOs/AlumnoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models.DTOs
+{
+    public class AlumnoDTO
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string email { get; set; }
+    }
+}
